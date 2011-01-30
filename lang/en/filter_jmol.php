9e40a428819b981108f31c60c4a7e649bf3b4daf
Geoff Rowland <geoff.rowland@yeovil.ac.uk>
2011-01-30 16:47:49 +0000

    Moving Jmol filter from CVS to Git Latest stable Jmol applet code Some minor tweaks for compatibility with Moodle 2.0  On branch master 	new file:   README.txt 	new file:   download.gif 	new file:   filter.php 	new file:   help.php 	new file:   jmol_help.html 	new file:   jmolapplet/CHANGES.txt 	new file:   jmolapplet/COPYRIGHT.txt 	new file:   jmolapplet/CVS/Entries 	new file:   jmolapplet/CVS/Repository 	new file:   jmolapplet/CVS/Root 	new file:   jmolapplet/Jmol.js 	new file:   jmolapplet/JmolApplet.jar 	new file:   jmolapplet/JmolApplet0.jar 	new file:   jmolapplet/JmolApplet0_Console.jar 	new file:   jmolapplet/JmolApplet0_Minimize.jar 	new file:   jmolapplet/JmolApplet0_Popup.jar 	new file:   jmolapplet/JmolApplet0_ReadersCifPdb.jar 	new file:   jmolapplet/JmolApplet0_ReadersMolXyz.jar 	new file:   jmolapplet/JmolApplet0_ReadersMore.jar 	new file:   jmolapplet/JmolApplet0_ReadersQuantum.jar 	new file:   jmolapplet/JmolApplet0_ReadersSimple.jar 	new file:   jmolapplet/JmolApplet0_ReadersXml.jar 	new file:   jmolapplet/JmolApplet0_ReadersXtal.jar 	new file:   jmolapplet/JmolApplet0_ShapeBio.jar 	new file:   jmolapplet/JmolApplet0_ShapeSpecial.jar 	new file:   jmolapplet/JmolApplet0_ShapeSurface.jar 	new file:   jmolapplet/JmolApplet0_Smiles.jar 	new file:   jmolapplet/JmolApplet0_Symmetry.jar 	new file:   jmolapplet/JmolApplet0_i18n_ar.jar 	new file:   jmolapplet/JmolApplet0_i18n_ca.jar 	new file:   jmolapplet/JmolApplet0_i18n_cs.jar 	new file:   jmolapplet/JmolApplet0_i18n_da.jar 	new file:   jmolapplet/JmolApplet0_i18n_de.jar 	new file:   jmolapplet/JmolApplet0_i18n_el.jar 	new file:   jmolapplet/JmolApplet0_i18n_en_GB.jar 	new file:   jmolapplet/JmolApplet0_i18n_es.jar 	new file:   jmolapplet/JmolApplet0_i18n_et.jar 	new file:   jmolapplet/JmolApplet0_i18n_fi.jar 	new file:   jmolapplet/JmolApplet0_i18n_fr.jar 	new file:   jmolapplet/JmolApplet0_i18n_hu.jar 	new file:   jmolapplet/JmolApplet0_i18n_id.jar 	new file:   jmolapplet/JmolApplet0_i18n_it.jar 	new file:   jmolapplet/JmolApplet0_i18n_ko.jar 	new file:   jmolapplet/JmolApplet0_i18n_nb.jar 	new file:   jmolapplet/JmolApplet0_i18n_nl.jar 	new file:   jmolapplet/JmolApplet0_i18n_oc.jar 	new file:   jmolapplet/JmolApplet0_i18n_pl.jar 	new file:   jmolapplet/JmolApplet0_i18n_pt.jar 	new file:   jmolapplet/JmolApplet0_i18n_pt_BR.jar 	new file:   jmolapplet/JmolApplet0_i18n_ru.jar 	new file:   jmolapplet/JmolApplet0_i18n_sl.jar 	new file:   jmolapplet/JmolApplet0_i18n_sv.jar 	new file:   jmolapplet/JmolApplet0_i18n_ta.jar 	new file:   jmolapplet/JmolApplet0_i18n_tr.jar 	new file:   jmolapplet/JmolApplet0_i18n_uk.jar 	new file:   jmolapplet/JmolApplet0_i18n_zh_CN.jar 	new file:   jmolapplet/JmolApplet0_i18n_zh_TW.jar 	new file:   jmolapplet/JmolData.jar 	new file:   jmolapplet/LEAME.txt 	new file:   jmolapplet/LICENSE.txt 	new file:   jmolapplet/README.txt 	new file:   lang/CVS/Entries 	new file:   lang/CVS/Repository 	new file:   lang/CVS/Root 	new file:   lang/en/CVS/Entries 	new file:   lang/en/CVS/Repository 	new file:   lang/en/CVS/Root 	new file:   lang/en/filter_jmol.php 	new file:   lang/en/help/CVS/Entries 	new file:   lang/en/help/CVS/Repository 	new file:   lang/en/help/CVS/Root 	new file:   lang/en/help/jmol/CVS/Entries 	new file:   lang/en/help/jmol/CVS/Repository 	new file:   lang/en/help/jmol/CVS/Root 	new file:   lang/en/help/jmol/jmol.html 	new file:   lang/fr/CVS/Entries 	new file:   lang/fr/CVS/Repository 	new file:   lang/fr/CVS/Root 	new file:   lang/fr/filter_jmol.php 	new file:   lang/fr/help/CVS/Entries 	new file:   lang/fr/help/CVS/Repository 	new file:   lang/fr/help/CVS/Root 	new file:   lang/fr/help/jmol/CVS/Entries 	new file:   lang/fr/help/jmol/CVS/Repository 	new file:   lang/fr/help/jmol/CVS/Root 	new file:   lang/fr/help/jmol/jmol.html 	new file:   manywater.pdb

---
+++ lang/en/filter_jmol.php
@@ -0,0 +1,14 @@
+<?php // $Id: filter_jmol.php,v 1.2 2010/08/06 00:18:12 geoffr Exp $
+// Language string for filter/jmol.
+ 
+$string['filtername'] = 'Jmol';
+$string['antialias'] = 'Antialias';
+$string['atoms'] = 'Atoms';
+$string['spin'] = 'Spin';
+$string['download'] = 'Download';
+$string['off'] = 'off';
+$string['unitcell'] = 'Unit Cell';
+$string['cartoon'] = 'Cartoon';
+$string['backbone'] = 'Backbone';
+$string['dwnlstructfile'] = 'Download the $a structure data file';
+$string['jsdisabled'] = 'The 3D viewer uses JavaScript to create the Java viewing window. If you are unable to make use of this technology, you can use the \"download\" link to download the file. If you are still unable to make use of the information, contact your teacher.';
---
+++ lang/fr/filter_jmol.php
@@ -0,0 +1,4 @@
+<?php // $Id: filter_jmol.php,v 1.1 2010/08/06 00:28:08 geoffr Exp $
+// Language string for filter/jmol.
+ 
+$string['filtername'] = 'Jmol';
