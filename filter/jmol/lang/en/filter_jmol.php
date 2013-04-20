c2a2af3aecf1d5ee807ef00ce4bf48413bd17c64
Geoff <geoff@localhost.localdomain>
2013-04-20 20:48:11 +0100

    Major update to use JSmol as YUI module

--- filter/jmol/lang/en/filter_jmol.php
+++ filter/jmol/lang/en/filter_jmol.php
@@ -1,14 +1,32 @@
-<?php // $Id: filter_jmol.php,v 1.2 2010/08/06 00:18:12 geoffr Exp $
-// Language string for filter/jmol.
+<?php 
+
+/**
+ * Strings for filter_jmol
+ *
+ * @package    filter
+ * @subpackage jmol
+ * @copyright  2006 Dan Stowell <david@moodle.com> Original version for Moodle 1.x
+ * @copyright  2010 Geoffrey Rowland <growland@strode-college.ac.uk> Update for Moodle 2.x
+ * @copyright  2013 Geoffrey Rowland <growland@strode-college.ac.uk> Update for JavaScript/HTML5 JSmol 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license    For Jmol/JSmol licence, see inside the bundled jmol and jsmol folders
+ */
  
 $string['filtername'] = 'Jmol';
 $string['antialias'] = 'Antialias';
-$string['atoms'] = 'Atoms';
+$string['Style'] = 'Style';
 $string['spin'] = 'Spin';
 $string['download'] = 'Download';
 $string['off'] = 'off';
-$string['unitcell'] = 'Unit Cell';
+$string['unitcell'] = 'Unit cell';
+$string['wireframe'] = 'Wireframe';
+$string['stick'] = 'Stick';
+$string['ballandstick'] = 'Ball and stick';
+$string['spacefill'] = 'Spacefill';
 $string['cartoon'] = 'Cartoon';
 $string['backbone'] = 'Backbone';
-$string['dwnlstructfile'] = 'Download the $a structure data file';
-$string['jsdisabled'] = 'The 3D viewer uses JavaScript to create the Java viewing window. If you are unable to make use of this technology, you can use the \"download\" link to download the file. If you are still unable to make use of the information, contact your teacher.';
+$string['hydrogens'] = 'Hydrogens';
+$string['polyhedra'] = 'Polyhedra';
+$string['jmolhelp'] = 'Jmol help';
+$string['downloadstructurefile'] = 'Download the chemical structure data file';
+$string['jsdisabled'] = 'The Jmol/JSmol 3D chemical structure viewer uses JavaScript. You may need to enable JavaScript in your Web browser. If you are unable to make use of this technology, you can still use the download link (below) to retrieve the chemical structure file.';
